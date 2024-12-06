d254a988e34abcdd202476e43dcd97bc51386e89
Thomas Torsney-Weir <torsneyt@gmail.com>
2024-12-06 10:42:56 +0100

    flake for development

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Flake for building qml firmware";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # from https://github.com/RyanGibb/qmk_firmware/blob/b9d97d2b49995a5647baade7cb343d7ee611d3e3/flake.nix
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        keyboard = "launch_3";
+        keymap = "ttw";
+      in
+      {
+        # build with `nix build '.?submodules=1'`
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "keymap";
+          src = ./.;
+          phases = [ "buildPhase" ];
+          buildInputs = [ pkgs.qmk ];
+          buildPhase = ''
+            make -C $src BUILD_DIR=`pwd`/.build COPY=echo -j8 $keyboard:$keymap
+            mkdir $out
+            cp -r .build/* $out/
+          '';
+        };
+
+        # Get a devshell with `nix develop` and then use `build` and `flash`.
+        # Build dir is taken as a param of `build` and `flash`,
+        # e.g. to flash with the output of `nix build '.?submodules=1'` do `flash result`.
+        devShell = pkgs.mkShell {
+          KEYBOARD = keyboard;
+          KEYMAP = keymap;
+          buildInputs = [ pkgs.qmk ];
+          shellHook = ''
+            build() {
+              BUILD_DIR=''${1:-.build}
+              make -C . BUILD_DIR=$BUILD_DIR COPY=echo -j8 $KEYBOARD:$KEYMAP
+            }
+            flash() {
+              BUILD_DIR=''${1:-.build}
+              make -C . BUILD_DIR=$BUILD_DIR COPY=echo -j8 $KEYBOARD:$KEYMAP:flash
+            }
+          '';
+        };
+      }
+    );
+}
